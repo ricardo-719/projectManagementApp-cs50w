c5b705b83977c70ed9a2ea3de515a7709021064c
ricardo-719 <teacherrickyricardo@gmail.com>
2023-03-22 17:22:54 +0700

    Implement security features

--- taskomaticFinalProjectCs50w/settings.py
+++ taskomaticFinalProjectCs50w/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,8 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r6ae)j=e2=j%y*1oy-khni+82zr^^*8231^y!%*l@r6&=pylgy'
+
+
+SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut away before
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
