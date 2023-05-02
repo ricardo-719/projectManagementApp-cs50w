5b0536c5dd873e9708f96543d1ba5b311e2ff910
ricardo-719 <teacherrickyricardo@gmail.com>
2023-05-02 16:38:48 +0700

    Separation of concerns by creating a refactoring forms.py

--- taskomatic/views.py
+++ taskomatic/views.py
@@ -3,75 +3,11 @@ from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
-from django.forms import ModelForm, TextInput, Textarea, CheckboxInput, DateInput
 from datetime import datetime
+from .forms import ProjectForm, TaskForm, InventoryForm
 from . import urls
 
 from .models import User, Project, Tasks, Inventory
-
-class ProjectForm(ModelForm):
-
-    class Meta:
-        model = Project
-        fields = "__all__"
-        widgets = {
-            'projectName': TextInput(attrs={
-            'class': "max-w-lg rounded border px-3 py-[0.32rem] mb-1",
-            'id': "projectTitleInput",
-            'placeholder': "Project Title"
-            }),
-            'projectDescription': Textarea(attrs={
-            'class': 'rounded px-3 py-[0.32rem] m-2',
-            'cols': "60",
-            'rows': "6",
-            'id': 'projectDescriptionInput',
-            'placeholder': "Project Description"
-            }),
-            'hasTasks': CheckboxInput(),
-            'hasInventory': CheckboxInput(),
-            'hasDeadline': CheckboxInput(attrs={
-                'id': "projectDeadlineCheckbox",
-                'name': "projectDeadlineCheckbox"
-            }),
-            'deadlineDate': DateInput(attrs=dict(type='date'))
-        }
-
-class TaskForm(ModelForm):
-
-    class Meta:
-        model = Tasks
-        fields = "__all__"
-        widgets = {
-            "taskName": TextInput(attrs={
-            'class': "max-w-lg rounded border px-3 py-[0.32rem] mb-1",
-            'placeholder': "Project Title"
-            }),
-            "taskDescription": Textarea(attrs={
-            'class': 'rounded px-3 py-[0.32rem] m-2',
-            'cols': "60",
-            'rows': "6",
-            'placeholder': "Task details"
-            })
-        }
-
-class InventoryForm(ModelForm):
-
-    class Meta:
-        model = Inventory
-        fields = "__all__"
-        widgets = {
-            "itemName": TextInput(attrs={
-            'class': "max-w-lg rounded border px-3 py-[0.32rem] mb-1",
-            'placeholder': "Project Title"
-            }),
-            "itemDescription": Textarea(attrs={
-            'class': 'rounded px-3 py-[0.32rem] m-2',
-            'cols': "60",
-            'rows': "6",
-            'placeholder': "Item notes"
-            })
-        }
-
 
 def index(request):
     projects = Project.objects.all().order_by('-creationDate')
